659071a95aa35d74b72c700d689ad2ade2bf06f9
ArjunPimpale <pimpalearjun10@gmail.com>
2024-07-16 14:32:12 +0530

    added sensor values

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ const apiKey = 'SX2MQRH2IHLWECII';
 
 // Function to fetch sensor data from ThingSpeak API
 async function fetchSensorData() {
-  const url = 'https://api.thingspeak.com/channels/2599999/fields/1.json?api_key=SX2MQRH2IHLWECII&results=2';
+  const url = 'https://api.thingspeak.com/channels/2599999/feeds.json?api_key=SX2MQRH2IHLWECII&results=1';
 
   try {
     const response = await fetch(url);
     const data = await response.json();
     // console.log(data)
-    console.log(data.feeds[0].field1)
+    let phsensor = data.feeds[0].field1;
+    let Color = data.feeds[0].field2;
+    let Turb = data.feeds[0].field3;
+    let methane = data.feeds[0].field4;
+    let alcohol = data.feeds[0].field5;
+
+    document.querySelector("#phsensor").innerHTML = `${phsensor}`
+    document.querySelector("#colorsensor").innerHTML = `${Color}`
+    document.querySelector("#turbidity").innerHTML = `${Turb}`
+    document.querySelector("#methane").innerHTML = `${methane}`
+    document.querySelector("#alcohol").innerHTML = `${alcohol}`
+
+    console.log(data.feeds[0].field2)
+    
     // console.log("Sensor value:", data.field1); // Access the sensor value from the response
   } catch (error) {
     console.error("Error fetching data:", error);
