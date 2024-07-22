13ca482772bf18b6d775a1462ea39dbba8401138
ArjunPimpale <pimpalearjun10@gmail.com>
2024-07-22 22:42:53 +0530

    MUDA MUDA MUDA

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
   // Replace with your actual ThingSpeak Channel ID and Read API Key
   const channelId = '2599999';
   const apiKey = 'SX2MQRH2IHLWECII';
 
   // Function to fetch sensor data from ThingSpeak API
   async function fetchSensorData() {
-    const url = `https://api.thingspeak.com/channels/${channelId}/feeds.json?api_key=${apiKey}&results=1`;
+    const url = `https://blynk.cloud/external/api/getAll?token=J5sf8xbUSsCBJ6Ron8pnFReQvb04G_F8`;
 
     try {
       const response = await fetch(url);
       const data = await response.json();
-      let phsensor = data.feeds[0].field1;
-      let color = data.feeds[0].field2;
-      let turb = data.feeds[0].field3;
-      let methane = data.feeds[0].field4;
-      let alcohol = data.feeds[0].field5;
+      let phsensor = data.v1;
+      let color = data.v3;
+      let turb = data.v0;
+      let methane = data.v32;
+      let alcohol = data.v2;
 
       document.querySelector("#turbidity").innerHTML = `${turb}`;
       document.querySelector("#phsensor").innerHTML = `${phsensor}`;
@@ -22,15 +22,15 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelector("#methane").innerHTML = `${methane}`;
       document.querySelector("#alcohol").innerHTML = `${alcohol}`;
 
-      console.log(data.feeds[0]);
+      console.log(data.v0);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   }
 
   // Fetch data every 15 seconds
-  setInterval(fetchSensorData, 1000);
+  setInterval(fetchSensorData, 500);
 
   // Initial call to fetch data immediately when the page loads
   fetchSensorData();
-});
+// });
