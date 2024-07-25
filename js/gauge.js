913149f1f25d83987f2c6a0b99b1081f022435ab
ArjunPimpale <pimpalearjun10@gmail.com>
2024-07-25 14:57:55 +0530

    added alcohol,methane,turbidity values

--- js/gauge.js
+++ js/gauge.js
@@ -1,5 +1,4 @@
 $(function () {
-
   class GaugeChart {
     constructor(element, params) {
       this._element = element;
@@ -15,50 +14,61 @@ $(function () {
       return {
         value: this._initialValue,
         valueIndicator: {
-          color: '#fff' },
+          color: "#fff",
+        },
 
         geometry: {
           startAngle: 180,
-          endAngle: 360 },
+          endAngle: 360,
+        },
 
         scale: {
           startValue: 0,
           endValue: this._higherValue,
           customTicks: [0, 250, 500, 780, 1050, 1300, 1560],
           tick: {
-            length: 8 },
+            length: 8,
+          },
 
           label: {
             font: {
-              color: '#87959f',
+              color: "#87959f",
               size: 9,
-              family: '"Open Sans", sans-serif' } } },
+              family: '"Open Sans", sans-serif',
+            },
+          },
+        },
 
         title: {
-          verticalAlignment: 'bottom',
+          verticalAlignment: "bottom",
           text: this._title,
           font: {
             family: '"Open Sans", sans-serif',
-            color: '#fff',
-            size: 10 },
+            color: "#fff",
+            size: 10,
+          },
 
           subtitle: {
             text: this._subtitle,
             font: {
               family: '"Open Sans", sans-serif',
-              color: '#fff',
+              color: "#fff",
               weight: 700,
-              size: 28 } } },
+              size: 28,
+            },
+          },
+        },
 
         onInitialized: function () {
           let currentGauge = $(element);
-          let circle = currentGauge.find('.dxg-spindle-hole').clone();
-          let border = currentGauge.find('.dxg-spindle-border').clone();
+          let circle = currentGauge.find(".dxg-spindle-hole").clone();
+          let border = currentGauge.find(".dxg-spindle-border").clone();
 
-          currentGauge.find('.dxg-title text').first().attr('y', 48);
-          currentGauge.find('.dxg-title text').last().attr('y', 28);
-          currentGauge.find('.dxg-value-indicator').append(border, circle);
-        } };
+          currentGauge.find(".dxg-title text").first().attr("y", 48);
+          currentGauge.find(".dxg-title text").last().attr("y", 28);
+          currentGauge.find(".dxg-value-indicator").append(border, circle);
+        },
+      };
     }
 
     init() {
@@ -67,30 +77,58 @@ $(function () {
   }
 
   $(document).ready(function () {
-
-    $('.gauge').each(function (index, item) {
+    $(".gauge").each(function (index, item) {
       let params = {
         initialValue: 780,
         higherValue: 1560,
         title: `Temperature ${index + 1}`,
-        subtitle: '780 ºC' };
+        subtitle: "780 ºC",
+      };
 
       let gauge = new GaugeChart(item, params);
       gauge.init();
     });
 
     function updateGauges() {
-      $('.gauge').each(function (index, item) {
-        let gauge = $(item).dxCircularGauge('instance');
+      $("#gauge1").each(function (index, item) {
+        let gauge = $(item).dxCircularGauge("instance");
         const methaneValue = getSensorData.methane; // Access methane value
         let gaugeElement = $(gauge._$element[0]);
         const numberElement = document.getElementById("alcohol");
-const numberString = numberElement.innerHTML;
-const numberInt = parseInt(numberString)*0.3809;
+        const numberString = numberElement.innerHTML;
+        const numberInt = parseInt(numberString) * 0.3809;
 
-console.log(numberInt); // This will output the number as an integer
-        
-        gaugeElement.find('.dxg-title text').last().html(`${methaneValue} ºC`);
+        console.log(numberInt); // This will output the number as an integer
+
+        gaugeElement.find(".dxg-title text").last().html(`${methaneValue} ºC`);
+        gauge.value(numberInt);
+      });
+
+      $("#gauge2").each(function (index, item) {
+        let gauge = $(item).dxCircularGauge("instance");
+        const methaneValue = getSensorData.methane; // Access methane value
+        let gaugeElement = $(gauge._$element[0]);
+        const numberElement = document.getElementById("methane");
+        const numberString = numberElement.innerHTML;
+        const numberInt = parseInt(numberString) * 0.3809;
+
+        console.log(numberInt); // This will output the number as an integer
+
+        gaugeElement.find(".dxg-title text").last().html(`${methaneValue} ºC`);
+        gauge.value(numberInt);
+      });
+
+      $("#gauge3").each(function (index, item) {
+        let gauge = $(item).dxCircularGauge("instance");
+        const methaneValue = getSensorData.methane; // Access methane value
+        let gaugeElement = $(gauge._$element[0]);
+        const numberElement = document.getElementById("turbidity");
+        const numberString = numberElement.innerHTML;
+        const numberInt = parseInt(numberString) * 0.3809;
+
+        console.log(numberInt); // This will output the number as an integer
+
+        gaugeElement.find(".dxg-title text").last().html(`${methaneValue} ºC`);
         gauge.value(numberInt);
       });
     }
