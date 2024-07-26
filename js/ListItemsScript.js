b77a2bb31f16b40e477846ac4eb147db9a621f10
ArjunPimpale <pimpalearjun10@gmail.com>
2024-07-26 23:23:02 +0530

    shortened the code

--- js/ListItemsScript.js
+++ js/ListItemsScript.js
@@ -1,3 +1,10 @@
+
+for(let i = 0;i<16;i++){
+  const items = document.createElement("div")
+  items.classList.add("item")
+  document.querySelector(".choice-container").appendChild(items)
+}
+
 document.querySelectorAll(".item").forEach(element => {
   element.innerHTML = `<img src="css/svgs/banana.svg" alt="banana">`
 });
